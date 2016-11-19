4601092f81c0f0a365daa1be139dce51168a1e2a
jyk88 <jiyoon.koo@gmail.com>
2016-11-19 18:19:15 -0500

    completed sql script as was drawn and discussed by team...some more work to do

--- CutePuppies/scripts/CutePuppies.sql
+++ CutePuppies/scripts/CutePuppies.sql
@@ -36,8 +36,116 @@ ArchivedOnDate DATE NOT NULL,
 PRIMARY KEY (PostId)
 );
 
+CREATE TABLE User (
+UserId INT NOT NULL AUTO_INCREMENT,
+RoleID INT NOT NULL,
+CreatedDate DATE NOT NULL,
+UpdatedDate DATE,
+DeletedDate DATE,
+UserName VARCHAR(100),
+UserPassword VARCHAR(100),
+PRIMARY KEY (UserId),
+KEY (RoleId)
+);
+
 CREATE TABLE `Status` (
 StatusId INT NOT NULL AUTO_INCREMENT,
 StatusDescription VARCHAR(100),
 PRIMARY KEY (StatusId)
 );
+INSERT INTO `Status` (StatusDescription) VALUES ('Published');
+INSERT INTO `Status` (StatusDescription) VALUES ('Draft');
+INSERT INTO `Status` (StatusDescription) VALUES ('Awaiting approval');
+INSERT INTO `Status` (StatusDescription) VALUES ('Archived');
+
+CREATE TABLE Role (
+RoleId INT NOT NULL AUTO_INCREMENT,
+RoleDescription VARCHAR(100),
+PRIMARY KEY (RoleId)
+);
+INSERT INTO Role (RoleDescription) VALUES ('Administrator');
+INSERT INTO Role (RoleDescription) VALUES ('Author');
+INSERT INTO Role (RoleDescription) VALUES ('Visitor');
+INSERT INTO Role (RoleDescription) VALUES ('Member');
+
+CREATE TABLE ContentType (
+ContentTypeId INT NOT NULL AUTO_INCREMENT,
+ContentTypeDescription VARCHAR(100),
+PRIMARY KEY (ContentTypeId)
+);
+INSERT INTO ContentType (ContentTypeDescription) VALUES ('Post');
+INSERT INTO ContentType (ContentTypeDescription) VALUES ('Static page');
+INSERT INTO ContentType (ContentTypeDescription) VALUES ('Comment');
+
+CREATE TABLE Permission (
+PermissionId INT NOT NULL AUTO_INCREMENT,
+PermissionDescription VARCHAR(100),
+PRIMARY KEY (PermissionId)
+);
+INSERT INTO Permission (PermissionDescription) VALUES ('Can Create New Draft');
+INSERT INTO Permission (PermissionDescription) VALUES ('Can Edit Published And Draft Post');
+INSERT INTO Permission (PermissionDescription) VALUES ('Can Publish Post');
+INSERT INTO Permission (PermissionDescription) VALUES ('Can Delete Post');
+INSERT INTO Permission (PermissionDescription) VALUES ('Can Submit Draft To Publish');
+INSERT INTO Permission (PermissionDescription) VALUES ('Can Submit Post To Delete');
+
+CREATE TABLE Tag (
+TagId INT NOT NULL AUTO_INCREMENT,
+TagDescription VARCHAR(500),
+PRIMARY KEY (TagId)
+);
+INSERT INTO Tag (TagDescription) VALUES ('Adoption');
+INSERT INTO Tag (TagDescription) VALUES ('Training');
+INSERT INTO Tag (TagDescription) VALUES ('Advice');
+
+CREATE TABLE Category (
+CategoryId INT NOT NULL AUTO_INCREMENT,
+CategoryDescription VARCHAR(500),
+PRIMARY KEY (CategoryId)
+);
+INSERT INTO Category (CategoryDescription) VALUES ('Ask An Expert');
+INSERT INTO Category (CategoryDescription) VALUES ('Adoption Story');
+INSERT INTO Category (CategoryDescription) VALUES ('Animal Rescue');
+
+-- establishing foreign keys for content table
+ALTER TABLE Content
+ADD CONSTRAINT User_fk_Post FOREIGN KEY (PostId) REFERENCES Post (PostId) ON DELETE NO ACTION,
+ADD CONSTRAINT User_fk_Status FOREIGN KEY (StatusId) REFERENCES `Status` (StatusId) ON DELETE NO ACTION;
+
+-- establishing foreign key for user table
+ALTER TABLE User
+ADD CONSTRAINT User_fk_Role FOREIGN KEY (RoleId) REFERENCES Role (RoleId) ON DELETE NO ACTION;
+
+-- establishing bridge table betwen role and permission
+CREATE TABLE role_permission (
+RoleId INT NOT NULL,
+PermissionId INT NOT NULL,
+KEY RoleId (RoleId),
+KEY PermissionId (PermissionId)
+);
+ALTER TABLE role_permission
+ADD CONSTRAINT role_permission_fk_role FOREIGN KEY (RoleId) REFERENCES Role (RoleId) ON DELETE NO ACTION,
+ADD CONSTRAINT role_permission_fk_permission FOREIGN KEY (PermissionId) REFERENCES Permission (PermissionId) ON DELETE NO ACTION;
+
+-- establishing bridge table between content and tag
+CREATE TABLE content_tag (
+ContentId INT NOT NULL,
+TagId INT NOT NULL,
+KEY ContentId (ContentId),
+KEY TagId (TagId)
+);
+ALTER TABLE content_tag
+ADD CONSTRAINT content_tag_fk_content FOREIGN KEY (ContentId) REFERENCES Content (ContentId) ON DELETE NO ACTION,
+ADD CONSTRAINT content_tag_fk_tag FOREIGN KEY (TagId) REFERENCES Tag (TagId) ON DELETE NO ACTION;
+
+-- establishing bridge table between content and category
+CREATE TABLE content_category (
+ContentId INT NOT NULL,
+CategoryId INT NOT NULL,
+KEY ContentId (ContentId),
+KEY CategoryId (CategoryId)
+);
+ALTER TABLE content_category
+ADD CONSTRAINT content_category_fk_content FOREIGN KEY (ContentId) REFERENCES Content (ContentId) ON DELETE NO ACTION,
+ADD CONSTRAINT content_category_fk_category FOREIGN KEY (CategoryId) REFERENCES Category (CategoryId) ON DELETE NO ACTION;
+
